cb2094931be446f84cae275eed7962d9f335d5f7
s0nnarr <s0narr@protonmail.com>
2025-02-21 16:02:27 +0200

    Initial commit -> Added newsniff_01

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+def fetch_page(url):
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+    }
+
+def main():
+    r = requests.get('https://quotes.toscrape.com/')
+    print(r)
+    print(r.content)
+
+main()
